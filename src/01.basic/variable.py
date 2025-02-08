08bb76b965c7a35f3616bce5e50b8dfba8e8c36f
saiket-das <saiketdas02@gmail.com>
2025-02-08 13:06:10 +0800

    Initial commit

---
+++ src/01.basic/variable.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+
+# String 
+name: str = 'Ahan Bryan'
+print(name)
+
+# Integer 
+age: int = 30  
+print(age)
+
+# Float or Double
+PI = 3.1416
+print(PI)
+
+# Boolean
+is_active: bool = True
+print(is_active)
+
+
+# Name, age and is_new_patient
+name: str = input("Enete your name: ")
+age: int = input("Enete your age: ")
+is_new_patient: bool = input("Are your new patien? ")
+
+print("Name: " + name + "Age: " + age + " and New patient: " + is_new_patient)
